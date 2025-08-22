3162bd762fa19fc0ddfa7c1299ec9f1d0adbc522
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-22 16:15:33 -0300

    criando tabela de cadastro de insumos

--- create_insumos.php
+++ create_insumos.php
@@ -28,5 +28,47 @@
         <input type="submit" value="Cadastrar Insumos">
 
     </form>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>Id</th>
+            <th>Atividade</th>
+            <th>Pessoa Atribuida</th>
+            <th>Status</th>
+            <th>Insumos</th>
+            <th>Ações</th>
+        </tr>
+
+        <?php if(mysqli_num_rows($result)>0): ?>
+            <?php while($row=mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?php echo $row['id']?></td>
+                    <td><?php echo $row['atividade']?></td>
+                    <td><?php echo $row['usuario']; ?></td>
+                    <td><?php echo $row['producao']?></td>
+                    <td><?php
+                    
+                            $resultInsumos = mysqli_query($conn, "SELECT * FROM atividade_insumos as ai JOIN insumos as i ON i.id=ai.insumos_id where ai.atividade_id=".$row['id']);
+                            while($atvInsumos=mysqli_fetch_assoc($resultInsumos)){
+                                echo $atvInsumos['descricao_insumos']."<br>";
+                            }
+                        ?>
+
+                    </td>
+                    <td>
+                         <a href="atribuir_insumos.php?id=<?php echo $row['id']?>">Atribuir Insumos</a><br>
+                        <a href="atribuir_usuario.php?id=<?php echo $row['id']?>">Atribuir Usuário</a><br>
+
+                        <a href="edit_atividade.php?id=<?php echo $row['id']?>">Editar</a><br>
+                        <a href="delete_atividade.php?id=<?php echo $row['id']?>" onclick="return confirm('Tem certeza de que deseja exlcuir a atividade?')">Excluir</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="4">Nenhuma atividade encontrada!</td>
+                </tr>
+        <?php endif; ?>
+
+    </table>
 </body>
 </html>
