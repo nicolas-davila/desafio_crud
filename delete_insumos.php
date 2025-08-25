8f372e4039c5ba7fc3f7d4cb07adb7e9a464ecda
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-25 10:40:44 -0300

    testando delete de insumos com condição

--- delete_insumos.php
+++ delete_insumos.php
@@ -3,7 +3,23 @@
     include("db.php");
 
     $id = $_GET["id"]; // Pega o id que vem pela URL
-    mysqli_query($conn, "DELETE FROM insumos WHERE id = $id"); //Executa a query de delete
-    header("location: create_insumos.php")
+    $result = mysqli_query($conn, "SELECT * FROM atividade_insumos WHERE insumos_id = $id");
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "<script>
+        
+                alert('Não é possível remover insumo, pois ele está atribuido a uma ou mais atividades.);
+                window.location.href='create_insumos.php';
+
+            </script>";
+    } else {
+        mysqli_query($conn, "DELETE FROM insumos WHERE id = $id");
+        echo "<script>
+        
+                alert('Insumo excluido com sucesso!');
+                window.loaction.href='create_insumos.php';
+
+            </script>";
+    }
 
 ?>
