856059941478f167d1a14afd5f545dd62c9b45bd
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-25 08:17:17 -0300

    Criação de tabela do usuário

--- create_usuario.php
+++ create_usuario.php
@@ -11,6 +11,7 @@
         header("Location: index.php");
     }
 
+    $result = mysqli_query($conn, "SELECT * FROM usuarios")
 ?>
 
 <!DOCTYPE html>
@@ -26,5 +27,30 @@
         <input type="text" name="nome" placeholder="Nome do Usuário" required>
         <input type="submit" value="Cadastrar Usuário">
     </form>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>Id</th>
+            <th>Usuários Cadastrados</th>
+            <th>Ações</th>
+        </tr>
+
+        <?php if(mysqli_num_rows($result)>0): ?>
+            <?php while($row=mysqli_fetch_assoc($result)): ?>
+                <tr>
+                    <td><?php echo $row['id']?></td>
+                    <td><?php echo $row['nome']?></td>
+                    <td>
+                        <a href="edit_insumos.php?id=<?php echo $row['id']?>">Editar usuário</a><br>
+                        <a href="delete_insumos.php?id=<?php echo $row['id']?>" onclick="return confirm('Tem certeza de que deseja exlcuir a atividade?')">Excluir usuário</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="4">Nenhum usuário encontrado!</td>
+                </tr>
+        <?php endif; ?>
+
+    </table>
 </body>
 </html>
