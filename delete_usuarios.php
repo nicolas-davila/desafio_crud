f58f98f0e5a80ce507026c9d300e0d9e11b01388
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2025-08-25 10:30:40 -0300

    Delete usuário funcional! (apenas adicionei um alert antes de redirecionar)

--- delete_usuarios.php
+++ delete_usuarios.php
@@ -5,14 +5,19 @@
     $id = $_GET["id"];
     $result = mysqli_query($conn, "SELECT * FROM atividades WHERE usuario_atribuido=$id");
 
-    var_dump($result);
+    // var_dump($result);
 
     if(mysqli_num_rows($result) > 0) {
-        echo "<alert>Usuário não excluido, pois está atribuido a uma atividade!</alert>";
-        header("location: create_usuario.php");
+        echo "<script>
+                alert('Não é possível excluir o usuário, pois ele está atribuído a uma ou mais atividades.');
+                window.location.href='create_usuario.php';
+            </script>";
     } else {
         mysqli_query($conn, "DELETE FROM usuarios WHERE id = $id");
-        header("location: create_usuario.php");
+        echo "<script>
+                alert('Usuário excluido com sucesso.');
+                window.location.href='create_usuario.php';
+            </script>";
     }
 
 ?>
